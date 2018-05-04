bfee0958ded827d1bb00b0d5337b5fa8ffe090af
Gonzalez <0566184@students.dadeschools.net>
2018-05-04 14:00:17 -0400

    fixed

---
+++ assets/javascript/app.js
@@ -0,0 +1,166 @@
+
+var panel = $("#quiz-area");
+var countStartNumber = 30;
+
+// Question set
+var questions = [{
+  question: "What is the biggest passenger aircraft?",
+  answers: ["Boeing 747", "Airbus A380", "Boeing 787", "Airbus A320"],
+  correctAnswer: "Airbus A380",
+  image: "assets/images/Airbus A380.jpg"
+}, {
+  question: "What is the most common training aircraft?",
+  answers: ["Cessna Citation", "Boeing 737", "Cessna 172", "Piper Cub"],
+  correctAnswer: "Cessna 172",
+  image: "assets/images/Cessna 172.jpg"
+}, {
+  question: "Which airplane has the longest range?",
+  answers: ["MD-11", "Cessna Caravan", "Airbus A350", "Boeing 787"],
+  correctAnswer: "Boeing 787",
+  image: "assets/images/Boeing 787.jpg"
+}, {
+  question: "What fighter jet is currently the most advanced fighter plane in the US military?",
+  answers: ["F22 Raptor", "F/A 18 Hornet", "F16 Falcon", "F35 Lighting"],
+  correctAnswer: "F35 Lighting",
+  image: "assets/images/F35 Lightning.jpg"
+}];
+
+// Variable to hold our setInterval
+var timer;
+
+var game = {
+
+  questions: questions,
+  currentQuestion: 0,
+  counter: countStartNumber,
+  correct: 0,
+  incorrect: 0,
+
+  countdown: function() {
+    game.counter--;
+    $("#counter-number").html(game.counter);
+    if (game.counter === 0) {
+      console.log("TIME UP");
+      game.timeUp();
+    }
+  },
+
+  loadQuestion: function() {
+
+    timer = setInterval(game.countdown, 1000);
+
+    panel.html("<h2>" + questions[this.currentQuestion].question + "</h2>");
+
+    for (var i = 0; i < questions[this.currentQuestion].answers.length; i++) {
+      panel.append("<button class='answer-button' id='button' data-name='" + questions[this.currentQuestion].answers[i]
+      + "'>" + questions[this.currentQuestion].answers[i] + "</button>");
+    }
+  },
+
+  nextQuestion: function() {
+    game.counter = countStartNumber;
+    $("#counter-number").html(game.counter);
+    game.currentQuestion++;
+    game.loadQuestion();
+  },
+
+  timeUp: function() {
+
+    clearInterval(timer);
+
+    $("#counter-number").html(game.counter);
+
+    panel.html("<h2>Out of Time!</h2>");
+    panel.append("<h3>The Correct Answer was: " + questions[this.currentQuestion].correctAnswer);
+    panel.append("<img src='" + questions[this.currentQuestion].image + "' />");
+
+    if (game.currentQuestion === questions.length - 1) {
+      setTimeout(game.results, 3 * 1000);
+    }
+    else {
+      setTimeout(game.nextQuestion, 3 * 1000);
+    }
+  },
+
+  results: function() {
+
+    clearInterval(timer);
+
+    panel.html("<h2>All done, heres how you did!</h2>");
+
+    $("#counter-number").html(game.counter);
+
+    panel.append("<h3>Correct Answers: " + game.correct + "</h3>");
+    panel.append("<h3>Incorrect Answers: " + game.incorrect + "</h3>");
+    panel.append("<h3>Unanswered: " + (questions.length - (game.incorrect + game.correct)) + "</h3>");
+    panel.append("<br><button id='start-over'>Start Over?</button>");
+  },
+
+  clicked: function(e) {
+    clearInterval(timer);
+    if ($(e.target).attr("data-name") === questions[this.currentQuestion].correctAnswer) {
+      this.answeredCorrectly();
+    }
+    else {
+      this.answeredIncorrectly();
+    }
+  },
+
+  answeredIncorrectly: function() {
+
+    game.incorrect++;
+
+    clearInterval(timer);
+
+    panel.html("<h2>Nope!</h2>");
+    panel.append("<h3>The Correct Answer was: " + questions[game.currentQuestion].correctAnswer + "</h3>");
+    panel.append("<img src='" + questions[game.currentQuestion].image + "' />");
+
+    if (game.currentQuestion === questions.length - 1) {
+      setTimeout(game.results, 3 * 1000);
+    }
+    else {
+      setTimeout(game.nextQuestion, 3 * 1000);
+    }
+  },
+
+  answeredCorrectly: function() {
+
+    clearInterval(timer);
+
+    game.correct++;
+
+    panel.html("<h2>Correct!</h2>");
+    panel.append("<img src='" + questions[game.currentQuestion].image + "' />");
+
+    if (game.currentQuestion === questions.length - 1) {
+      setTimeout(game.results, 3 * 1000);
+    }
+    else {
+      setTimeout(game.nextQuestion, 3 * 1000);
+    }
+  },
+
+  reset: function() {
+    this.currentQuestion = 0;
+    this.counter = countStartNumber;
+    this.correct = 0;
+    this.incorrect = 0;
+    this.loadQuestion();
+  }
+};
+
+// CLICK EVENTS
+
+$(document).on("click", "#start-over", function() {
+  game.reset();
+});
+
+$(document).on("click", ".answer-button", function(e) {
+  game.clicked(e);
+});
+
+$(document).on("click", "#start", function() {
+  $("#sub-wrapper").prepend("<h2>Time Remaining: <span id='counter-number'>30</span> Seconds</h2>");
+  game.loadQuestion();
+});
